436d9f3d8522deef2d10721a36f0c51b84c40021
Du06Ag <39678678+Du06Ag@users.noreply.github.com>
2018-06-15 02:21:25 -0500

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 // configuracion de la base de datos
 const mysql = require('mysql');
 module.exports = mysql.createConnection({
-	host: 'bwncddiiv-mysql.services.clever-cloud.com',
-	user: 'uatpbisj2zqyrwbx',
-	password: 'dGVhzaeo6Kv5nV5SY0j',
-	database: 'bwncddiiv',
+	host: '127.0.0.1',
+	user: 'root',
+	password: 'duky',
+	database: 'sistema_servibus_test',
 	port: 3306
 });
